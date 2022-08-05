7f6ce8017f9f433e2acde966126aa200e6580a35
YaserAlqasem <alqasemyaser@gmail.com>
2022-08-05 11:13:45 -0500

    Server.js

--- auction-backend/server.js
+++ auction-backend/server.js
@@ -1,23 +1,37 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const User = require("./models/User");
+const jsonwebtoken = require("jsonwebtoken");
 
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// mongodb connection
+mongoose.connect("mongodb://localhost:27017/AuctionDB", {
+  useNewUrlParser: true,
+});
+
+// user
+app.use("/users", require("./routers/UserRouter"));
 
 // auction
 
 // bidding
 
-// user
-    // sign up user
-    
+// Url not match
+app.use((req, res) => {
+  res.status(404).send({ url: req.originalUrl + " not found" });
+});
 
-// login
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: err });
+});
 
-//
-
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(3000, () => console.log("Listening on port 3000"));
