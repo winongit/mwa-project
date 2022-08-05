928c69a5aa2d1de982f4b24fab1c81183e30449a
YaserAlqasem <alqasemyaser@gmail.com>
2022-08-05 16:15:17 -0500

    AuthGuard

---
+++ auction-frontend/src/app/core/services/authservice/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor() {}
+
+  isLoggedIn() {
+    const token = localStorage.getItem('token'); // get token from local storage
+    if (token) {
+      const payload = atob(token.split('.')[1]); // decode payload of token
+      const parsedPayload = JSON.parse(payload); // convert payload into an Object
+
+      return parsedPayload.exp > Date.now() / 1000; // check if token is expired
+    }
+    return false;
+  }
+}
