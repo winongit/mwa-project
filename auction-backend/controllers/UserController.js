7cfad24313feb45fbf38f9e17925b89f035224c2
YaserAlqasem <alqasemyaser@gmail.com>
2022-08-05 11:15:46 -0500

    UserController

---
+++ auction-backend/controllers/UserController.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = mongoose.model("User");
+var userService = require("../services/UserService");
+
+exports.register = async (req, res) => {
+  try {
+    let user = await userService.registerUser(req.body);
+    return res.json(user);
+  } catch (err) {
+    return res.status(400).send({
+      message: err,
+    });
+  }
+};
