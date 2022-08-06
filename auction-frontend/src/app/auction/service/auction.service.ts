5708470cc149224375371f931e2b9a4cb741f24c
Win Htut Aung <winhtutaungster@gmail.com>
2022-08-06 13:51:17 -0500

    auction create with image upload

--- auction-frontend/src/app/auction/service/auction.service.ts
+++ auction-frontend/src/app/auction/service/auction.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Auction } from 'src/app/shared/models/auction';
 
 @Injectable({
@@ -8,7 +9,21 @@ import { Auction } from 'src/app/shared/models/auction';
 export class AuctionService {
   constructor(private http: HttpClient) { }
 
+  private baseUrl = 'http://localhost:3000'
+
   saveAuction(auction: Auction) {
-    return this.http.post('http://localhost:3000/auctions', auction);
+    return this.http.post(`${this.baseUrl}/auctions`, auction);
+  }
+
+  upload(file: File):  Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+
+    formData.append('picture', file);
+    const req = new HttpRequest('POST', `${this.baseUrl}/auctions/upload`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+
+    return this.http.request(req);
   }
 }
