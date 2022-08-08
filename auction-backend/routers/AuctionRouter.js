98c0f0814e09ba1aa08240e8cc38a6ac6c768c77
YaserAlqasem <alqasemyaser@gmail.com>
2022-08-08 17:01:39 -0500

    SignUp

--- auction-backend/routers/AuctionRouter.js
+++ auction-backend/routers/AuctionRouter.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const router = require("./UserRouter");
+const router = express.Router();
 const {
   createAuction,
   uploadPhoto,
   getAllAuctions,
+  getAuction,
 } = require("../controllers/AuctionController");
 
 const multer = require("multer");
 const path = require("path");
-const { checkToken } = require("../middlewares/checkToken");
-
-const maxFileSize = 5 * 1024 * 1024;
+const { fileFilterFunc } = require("../common/UploadPhotosConfig");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -23,31 +22,15 @@ const storage = multer.diskStorage({
   },
 });
 
-const fileFilterFunc = (req, file, cb) => {
-  const fileSize = parseInt(req.headers["content-length"]);
-  console.log("fileSize " + fileSize);
-  console.log(typeof file.mimetype);
-  console.log(file.mimetype === "image/jpeg");
-  if (fileSize >= maxFileSize) {
-    cb("bigfilesize", false);
-  } else if (
-    file.mimetype !== "image/jpg" &&
-    file.mimetype !== "image/jpeg" &&
-    file.mimetype !== "image/png"
-  ) {
-    cb("invalidfiletype", false);
-  } else {
-    cb(null, true);
-  }
-};
-
 const upload = multer({ storage: storage, fileFilter: fileFilterFunc });
 
 // (/auctions)
 router.get("/", checkToken, getAllAuctions);
 
-router.post("/", checkToken, createAuction);
+router.get("/:auction_id", getAuction);
 
-router.post("/upload", checkToken, upload.single("picture"), uploadPhoto);
+router.post("/", createAuction);
+
+router.post("/upload", upload.single("picture"), uploadPhoto);
 
 module.exports = router;
