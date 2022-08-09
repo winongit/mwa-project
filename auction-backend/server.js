82c17c6be063957cdb269be95ccd8afd62bd3165
YaserAlqasem <alqasemyaser@gmail.com>
2022-08-08 23:15:05 -0500

    MongoDB Atlas

--- auction-backend/server.js
+++ auction-backend/server.js
@@ -3,9 +3,9 @@ const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const Graceful = require('@ladjs/graceful');
-const Cabin = require('cabin');
-const Bree = require('bree');
+const Graceful = require("@ladjs/graceful");
+const Cabin = require("cabin");
+const Bree = require("bree");
 
 const { checkToken } = require("./middlewares/checkToken");
 
@@ -16,22 +16,26 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+require("dotenv").config();
 // mongodb connection
-mongoose.connect("mongodb://localhost:27017/AuctionDB", {
-  useNewUrlParser: true,
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected Successfully"))
+  .catch((err) => console.log(err));
 
 // middlewares
-app.use('/pictures', express.static(__dirname + '/assets/pics'));
+app.use("/pictures", express.static(__dirname + "/assets/pics"));
 
 // user
 app.use("/users", require("./routers/UserRouter"));
 
 // auction
-app.use("/auctions", checkToken, require('./routers/AuctionRouter'));
+app.use("/auctions", checkToken, require("./routers/AuctionRouter"));
 
 // bidding
-app.use("/bid", checkToken, require('./routers/BidRouter'));
+app.use("/bid", checkToken, require("./routers/BidRouter"));
 
 // Url not match
 app.use((req, res) => {
@@ -46,19 +50,17 @@ const bree = new Bree({
   logger: new Cabin(),
   jobs: [
     {
-      name: 'AuctionEngine',
-      interval: '1m'
-    }
-  ]
+      name: "AuctionEngine",
+      interval: "1m",
+    },
+  ],
 });
 
 const graceful = new Graceful({ brees: [bree] });
 
 (async () => {
   await bree.start();
-})()
+})();
 app.listen(3000, () => console.log("Listening on port 3000"));
 
 graceful.listen();
-
-
